ba58d0472d7ebb14ce21932984df3b3f76cb5e53
danieltmc <me@danieltmc.com>
2020-03-11 13:44:22 -0400

    Handle errors and batch updates

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
 const Ingredients = () => {
   const [ userIngredients, setUserIngredients ] = useState([]);
   const [ isLoading, setIsLoading ] = useState(false);
+  const [ error, setError ] = useState();
 
   useEffect(() => {
     console.log('[RENDERING INGREDIENTS]', userIngredients);
@@ -45,11 +47,19 @@ const Ingredients = () => {
       setUserIngredients(prevIngredients =>
         prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
       );
+    }).catch(error => {
+      setError(error.message);
     });
   };
 
+  const clearError = () => {
+    setError(null);
+    setIsLoading(false);
+  }
+
   return (
     <div className="App">
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
       <IngredientForm
         onAddIngredient={addIngredientHandler}
         loading={isLoading}
